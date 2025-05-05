3d434bfb24922dfa15a90f0ed9f1dde82d3200bb
Elias <eliascamilo70@outlook.com>
2025-05-05 19:47:09 -0300

    Criacao da tabela Usuario

---
+++ server/database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  tipo ENUM('morador', 'administrador') NOT NULL
+)
