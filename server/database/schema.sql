9d69e16f3867b8ed4714f87ff597fc14125fb96f
Elias <eliascamilo70@outlook.com>
2025-05-05 20:08:26 -0300

    Table comentarios

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -26,3 +26,13 @@ CREATE TABLE Votos (
   FOREIGN KEY (usuario_id) REFERENCES Usuarios(id)
   FOREIGN KEY (denuncia_id) REFERENCES Denuncias(id)
 )
+
+CREATE TABLE Comentarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT NOT NULL, 
+  denuncia_id INT NOT NULL,
+  texto TEXT NOT NULL,
+  data_comentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (usuario_id) REFERENCES Usuarios(id),
+  FOREIGN KEY (denuncia_id) REFERENCES Denuncias(id)
+)
