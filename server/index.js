82be22a63df07c5b55ec35fdf047b92ce5c11846
matheus4breu <matheusabreu36@gmail.com>
2025-05-07 22:24:29 -0300

    configuração do ambiente + conexao do banco de dados

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
 // Elias comentário
 // Matheus comentário
+const db = require("././db");
+console.log ('Start')
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
